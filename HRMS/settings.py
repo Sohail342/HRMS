52d822e26866391864b13ec97f9ebcf15b68b4a1
Sohail342 <sohailahmed34280@gmail.com>
2024-12-06 20:03:28 +0500

    update grading table

--- HRMS/settings.py
+++ HRMS/settings.py
@@ -101,6 +101,14 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 # Custom Employee Model
 AUTH_USER_MODEL = 'HRIS_App.Employee'
 
