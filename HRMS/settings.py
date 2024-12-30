277b8593d7382728635318a93ffa4e032a32e8ca
Sohail342 <sohailahmed34280@gmail.com>
2024-12-30 21:27:44 +0500

    update debug=true in production for testing

--- HRMS/settings.py
+++ HRMS/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-y6v#uewfea@x)o(5z&uemx@lrojs^*_in60ri38_pxn1!#@k@u'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
