93064d8f1505da891cf72b1b4e8e712d86248580
Sohail342 <sohailahmed34280@gmail.com>
2025-04-08 22:58:13 +0500

    adjustment of template footer

--- HRMS/urls.py
+++ HRMS/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,5 +15,5 @@ urlpatterns = [
     path('', include("group_head.urls")),
     path('', include("employee_user.urls")),
     path('', include("employee_attendance.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
