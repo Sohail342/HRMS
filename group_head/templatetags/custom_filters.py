2fe916d6f893043a150c109b648a1c2fde1f86ef
Sohail342 <sohailahmed34280@gmail.com>
2025-07-03 23:20:50 +0500

    Feature: Analytics App

--- group_head/templatetags/custom_filters.py
+++ group_head/templatetags/custom_filters.py
@@ -21,3 +21,17 @@ def add_pdf(url:str):
     if not url.endswith('.pdf'):
         return f'{url}.pdf'
     return url
+
+
+
+@register.filter
+def percentage_of(value, total):
+    """
+    Calculate what percentage of total is value
+    """
+    try:
+        if total == 0:
+            return 0
+        return int((float(value) / float(total)) * 100)
+    except (ValueError, ZeroDivisionError):
+        return 0
